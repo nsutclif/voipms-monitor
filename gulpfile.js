7a07ba772f74122cbdd47387459cf255f8cbcd68
Nathan Sutcliffe <nsutclif@gmail.com>
2017-06-06 06:24:03 -0700

    Attempt to upload to S3 during build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-// The easiest way I could see to zip files and directories in Lambda...
+// Lambda doesn't contain either aws cli or zip command
+// This was the easiest workaround I could find...
 
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const s3 = require('gulp-s3-upload')();
 
 gulp.task('package', () =>
   {
-    gulp.src('packaged/*')
+    gulp.src('packaged/**/*')
         .pipe(zip('packaged.zip'))
-        .pipe(gulp.dest('./'));
+        .pipe(s3({
+          Bucket: 'voip-ms-monitor-build-artifacts'
+        }));
   }
 );
