531fb9a38861865028d93b9fd090240f18725c04
Nathan Sutcliffe <nsutclif@gmail.com>
2017-06-06 20:38:25 -0700

    also upload template.yml

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,30 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 const s3 = require('gulp-s3-upload')();
 
+function prependKeyPrefix(relative_filename) {
+  return [ // TODO: Don't hard code this?
+    'gh',
+    process.env.LAMBCI_REPO,
+    'builds',
+    process.env.LAMBCI_BUILD_NUM,
+    relative_filename
+  ].join('/');
+}
+
+const BUCKET_NAME = 'lambci-buildresults-ga8wy7gvebrx'; // TODO: Don't hard-code this!
+
 gulp.task('package', () =>
   {
     gulp.src('packaged/**/*')
-        .pipe(zip('packaged.zip'))
-        .pipe(s3({
-          // TODO: Don't hard-code this!
-          Bucket: 'lambci-buildresults-ga8wy7gvebrx',
-          keyTransform: (relative_filename) => {
-            return [ // TODO: Don't hard code this?
-              'gh',
-              process.env.LAMBCI_REPO,
-              'builds',
-              process.env.LAMBCI_BUILD_NUM,
-              relative_filename
-            ].join('/');
-          }
-        }));
+      .pipe(zip('packaged.zip'))
+      .pipe(s3({
+        Bucket: BUCKET_NAME,
+        keyTransform: prependKeyPrefix
+      }));
+    gulp.src('template.yml')
+      .pipe(s3({
+        Bucket: BUCKET_NAME,
+        keyTransform: prependKeyPrefix
+      }));
   }
 );
