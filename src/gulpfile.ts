7653f4e1e8dbef9104e0294355f884eb7190c35e
Nathan Sutcliffe <nsutclif@gmail.com>
2017-10-13 07:18:59 -0700

    Initial changes to specify S3 locations with parameters

--- src/gulpfile.ts
+++ src/gulpfile.ts
@@ -4,11 +4,8 @@
 // Using Gulp was the easiest workaround I could find...
 
 import * as gulp from "gulp";
-import * as modifyFile from "gulp-modify-file";
 import * as s3 from "gulp-s3-upload";
 import * as zip from "gulp-zip";
-import * as yaml from "js-yaml";
-import * as VinylFile from "vinyl";
 
 const BUCKET_NAME = "lambci-buildresults-ga8wy7gvebrx"; // TODO: Don't hard-code this!
 const CODE_KEY_NAME = "PollVoipMSFunction.zip";
@@ -30,32 +27,6 @@ function constructS3URL(keyName: string): string {
     return ["https://s3.amazonaws.com", BUCKET_NAME, keyName].join("/");
 }
 
-function updateTemplateArtifactPaths(content: string, path: string, file: VinylFile): string {
-    const template: any = yaml.safeLoad(content);
-
-    Object.keys(template.Resources).filter((key) => {
-        return template.Resources[key].Type === "AWS::CloudFormation::Stack";
-    }).map((key) => {
-        const stackResource = template.Resources[key];
-
-        // Assume stackResource.Properties.TemplateURL already has the name of the template.
-        // We will prepend the rest of the URL.
-        stackResource.Properties.TemplateURL = constructS3URL(prependKeyPrefix(stackResource.Properties.TemplateURL));
-    });
-
-    Object.keys(template.Resources).filter((key) => {
-        return template.Resources[key].Type === "AWS::Lambda::Function";
-    }).map((key) => {
-        // TODO: Is there a type for functionResource?
-        const functionResource = template.Resources[key];
-
-        functionResource.Properties.Code.S3Bucket = BUCKET_NAME;
-        functionResource.Properties.Code.S3Key = prependKeyPrefix(CODE_KEY_NAME);
-    });
-
-    return yaml.safeDump(template);
-}
-
 gulp.task("package", () => {
     function logCompletedFile(keyname: string): void {
         console.log(constructS3URL(keyname));
@@ -68,7 +39,6 @@ gulp.task("package", () => {
             keyTransform: prependKeyPrefix,
         }));
     gulp.src("../cloudformation/*.yml")
-        .pipe(modifyFile(updateTemplateArtifactPaths))
         .pipe(s3()({
             Bucket: BUCKET_NAME,
             keyTransform: prependKeyPrefix,
